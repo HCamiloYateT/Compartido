ca5b302706977540e6d7b04d083feb5b3f10e3d1
Camilo Yate <89857691+HCamiloYateT@users.noreply.github.com>
2026-04-01 12:29:13 -0500

    Extiende Boton con tamaños, hover configurable y layout de etiqueta

--- bs4Dash/APP/R/functions/utils.R
+++ bs4Dash/APP/R/functions/utils.R
@@ -19,3 +19,45 @@ str_to_title <- function(texto) {
 formatear_booleano <- function(x) {
   ifelse(isTRUE(x), "Sí", "No")
 }
+
+# Botón reutilizable con icono/label -------------------------------------------
+# - size: xxs, xs, sm, md, lg, xl, xxl
+# - hover_color: color de fondo al hacer hover
+# - label_pos: "bottom" (debajo del icono) o "right" (a la derecha)
+Boton <- function(
+  input_id,
+  label,
+  icono = "floppy-disk",
+  size = c("md", "xxs", "xs", "sm", "lg", "xl", "xxl"),
+  hover_color = "firebrick",
+  label_pos = c("bottom", "right"),
+  class = NULL,
+  ...
+) {
+  size <- match.arg(size)
+  label_pos <- match.arg(label_pos)
+
+  orientacion_class <- if (identical(label_pos, "right")) {
+    "racafe-btn-guardar--horizontal"
+  } else {
+    "racafe-btn-guardar--vertical"
+  }
+
+  clases <- paste(
+    "racafe-btn-guardar",
+    paste0("racafe-btn-guardar--", size),
+    orientacion_class,
+    class
+  )
+
+  shiny::actionButton(
+    inputId = input_id,
+    label = shiny::tagList(
+      shiny::icon(icono, class = "racafe-btn-icon"),
+      shiny::span(class = "racafe-btn-label", label)
+    ),
+    class = clases,
+    style = paste0("--racafe-btn-hover:", hover_color, ";"),
+    ...
+  )
+}
