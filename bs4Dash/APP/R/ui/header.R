afb806319833e7dae4533f56525b963c791f4364
Camilo Yate <89857691+HCamiloYateT@users.noreply.github.com>
2026-04-07 11:29:25 -0500

    Delete bs4Dash directory

--- bs4Dash/APP/R/ui/header.R
+++
@@ -1,9 +0,0 @@
-# Header -----------------------------------------------------------------------
-# Construye la barra superior principal con título de la plantilla.
-
-header <- bs4DashNavbar(
-  skin = "light",
-  status = "white",
-  border = FALSE,
-  title = dashboardBrand(title = tit_app)
-)
--- bs4Dash/APP/R/ui/preloader.R
+++
@@ -1,4 +0,0 @@
-# Preloader --------------------------------------------------------------------
-# Archivo reservado para preloaders opcionales; se deja como plantilla mínima.
-
-preloader <- NULL
